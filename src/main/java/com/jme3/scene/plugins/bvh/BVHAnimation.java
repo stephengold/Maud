e9f85210bfaca4163b1f8adccd10a4577a2f314e
Stephen Gold <sgold@sonic.net>
2017-05-12 11:56:28 -0700

    comments

--- src/main/java/com/jme3/scene/plugins/bvh/BVHAnimation.java
+++ src/main/java/com/jme3/scene/plugins/bvh/BVHAnimation.java
@@ -1,12 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.jme3.scene.plugins.bvh;
 
 import java.util.logging.Logger;
 
 /**
+ * Summary information for a BVH asset.
  *
  * @author Nehon
  */
@@ -22,33 +19,79 @@ public class BVHAnimation {
     // *************************************************************************
     // fields
 
+    /**
+     * the root of the bone hierarchy
+     */
     private BVHBone hierarchy;
+    /**
+     * number of frames/samples in the motion (&ge;0)
+     */
     private int nbFrames;
+    /**
+     * time per frame/sample (in seconds, &gt;0)
+     */
     private float frameTime;
+    // *************************************************************************
+    // constructors
 
+    /**
+     * TODO redundant
+     */
     public BVHAnimation() {
     }
+    // *************************************************************************
+    // new methods exposed
 
+    /**
+     * Read the time per frame/sample.
+     *
+     * @return frame time (in seconds, &gt;0)
+     */
     public float getFrameTime() {
         return frameTime;
     }
 
+    /**
+     * Alter the time per frame/sample.
+     *
+     * @param frameTime frame time (in seconds, &gt;0)
+     */
     public void setFrameTime(float frameTime) {
         this.frameTime = frameTime;
     }
 
+    /**
+     * Access the bone hierarchy.
+     *
+     * @return the root bone (pre-existing instance)
+     */
     public BVHBone getHierarchy() {
         return hierarchy;
     }
 
+    /**
+     * Alter the bone hierarchy.
+     *
+     * @param hierarchy the root bone (alias created)
+     */
     public void setHierarchy(BVHBone hierarchy) {
         this.hierarchy = hierarchy;
     }
 
+    /**
+     * Read the number of frames/samples in the motion.
+     *
+     * @return count (&ge;0)
+     */
     public int getNbFrames() {
         return nbFrames;
     }
 
+    /**
+     * Alter the number of frames/samples in the motion.
+     *
+     * @param nbFrames count (&ge;0)
+     */
     public void setNbFrames(int nbFrames) {
         this.nbFrames = nbFrames;
     }
