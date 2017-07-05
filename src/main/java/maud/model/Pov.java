a2f650df97b33c4f7ae8ffa6eb2af05271d30bc9
Stephen Gold <sgold@sonic.net>
2017-07-05 12:54:06 -0700

    define a Pov interface

---
+++ src/main/java/maud/model/Pov.java
@@ -0,0 +1,75 @@
+/*
+ Copyright (c) 2017, Stephen Gold
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the copyright holder nor the names of its contributors
+ may be used to endorse or promote products derived from this software without
+ specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package maud.model;
+
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
+
+/**
+ * Interface to a camera in Maud's edit screen.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public interface Pov {
+    /**
+     * Copy the location of the camera.
+     *
+     * @param storeResult (modified if not null)
+     * @return world coordinates (either storeResult or a new vector)
+     */
+    Vector3f cameraLocation(Vector3f storeResult);
+
+    /**
+     * Copy the orientation of the camera.
+     *
+     * @param storeResult (modified if not null)
+     * @return rotation relative to world coordinates (either storeResult or a
+     * new instance)
+     */
+    Quaternion cameraOrientation(Quaternion storeResult);
+
+    /**
+     * Move the camera forward/backward when the scroll wheel is turned.
+     *
+     * @param amount scroll wheel notches
+     */
+    void moveBackward(float amount);
+
+    /**
+     * Move the camera left/right when the mouse is dragged from left/right.
+     *
+     * @param amount drag component
+     */
+    void moveLeft(float amount);
+
+    /**
+     * Move the camera up/down when the mouse is dragged up/down.
+     *
+     * @param amount drag component
+     */
+    void moveUp(float amount);
+}
