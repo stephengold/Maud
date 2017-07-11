724b45ed27d4a0349774d580e1ecf747e7ba1470
Stephen Gold <sgold@sonic.net>
2017-07-11 10:18:32 -0700

    Finial: add space between sparklines

--- src/main/java/maud/Finial.java
+++ src/main/java/maud/Finial.java
@@ -34,10 +34,16 @@ import com.jme3.util.BufferUtils;
 import java.nio.FloatBuffer;
 import java.nio.ShortBuffer;
 import java.util.logging.Logger;
+import jme3utilities.Validate;
 
 /**
- * A 2D, line-mode mesh used as an end cap for a staff. In local coordinates it
- * extends from -width to 0 in X and from -height to 0 in Y.
+ * A 2D, static, line-mode mesh intended to be used as an end cap for a staff. A
+ * finial can have up to 3 limbs (for translation, rotation, and scale). A
+ * branch has either 3 or 4 crowsfeet, with each foot marking 1 end of a
+ * sparkline.
+ * <p>
+ * In local coordinates, a finial extends from -width to 0 in X and from -height
+ * to 0 in Y.
  *
  * @author Stephen Gold sgold@sonic.net
  */
@@ -46,9 +52,33 @@ public class Finial extends Mesh {
     // constants and loggers
 
     /**
-     * width (in local units, &ge;0)
+     * height per foot (in local units)
      */
-    final private static double width = 0.05;
+    final public static double hpf = 0.1;
+    /**
+     * total width of a finial (in local units)
+     */
+    final public static double width = 0.05;
+    /**
+     * 1/3 of the total width (in local units)
+     */
+    final private static double otw = width / 3;
+    /**
+     * number of lines in a 3-foot limb
+     */
+    final private static int numLines3 = 10;
+    /**
+     * number of lines in a 4-foot limb
+     */
+    final private static int numLines4 = 14;
+    /**
+     * number of vertices in a 3-foot limb
+     */
+    final private static int numVertices3 = 15;
+    /**
+     * number of vertices in a 4-foot limb
+     */
+    final private static int numVertices4 = 22;
     /**
      * message logger for this class
      */
@@ -58,19 +88,23 @@ public class Finial extends Mesh {
     // fields
 
     /**
-     * how much vertical space the mesh occupies (in local units, &ge;0)
+     * vertical space occupied by completed limbs (in local units, &ge;0)
      */
     private double height = 0.0;
     /**
-     * buffer for positions, used during instantiation only
+     * half the vertical space each sparkline uses (in local units, &ge;0)
+     */
+    final private double shh;
+    /**
+     * buffer for position data, used during instantiation only
      */
     final private FloatBuffer floats;
     /**
-     * how many vertices have been generated, used during instantiation only
+     * number of vertices in the completed limbs
      */
     private int baseI = 0;
     /**
-     * buffer for indices, used during instantiation only
+     * buffer for index data, used during instantiation only
      */
     final private ShortBuffer shorts;
     // *************************************************************************
@@ -79,25 +113,31 @@ public class Finial extends Mesh {
     /**
      * Instantiate a mesh with the specified components.
      *
-     * @param tra if true, include 3 crowsfeet for translation offsets, else
-     * omit
-     * @param rot if true, include 4 crowsfeet for rotation elements, else omit
-     * @param sca if true, include 3 crowsfeet for scale factors, else omit
+     * @param tra if true, include a 3-foot limb for translations, else omit
+     * @param rot if true, include a 4-foot limb for rotations, else omit
+     * @param sca if true, include a 3-foot limb for scales, else omit
+     * @param sparklineHeight vertical space used by a sparkline (in local
+     * units, &ge;0)
      */
-    public Finial(boolean tra, boolean rot, boolean sca) {
+    public Finial(boolean tra, boolean rot, boolean sca,
+            float sparklineHeight) {
+        Validate.nonNegative(sparklineHeight, "sparkline height");
+
+        shh = sparklineHeight / 2;
+
         int numVertices = 0;
         int numLines = 1;
         if (tra) {
-            numVertices += 13;
-            numLines += 14;
+            numVertices += numVertices3;
+            numLines += numLines3;
         }
         if (rot) {
-            numVertices += 19;
-            numLines += 15;
+            numVertices += numVertices4;
+            numLines += numLines4;
         }
         if (sca) {
-            numVertices += 13;
-            numLines += 14;
+            numVertices += numVertices3;
+            numLines += numLines3;
         }
 
         floats = BufferUtils.createFloatBuffer(3 * numVertices);
@@ -112,15 +152,30 @@ public class Finial extends Mesh {
 
         populate(tra, rot, sca);
 
+        assert baseI == numVertices : numVertices;
+        assert positions.getNumElements() == numVertices : numVertices;
+        assert indices.getNumElements() == numLines : numLines;
+
         setMode(Mode.Lines);
         setStatic();
         updateBound();
+    }
+
+    /**
+     * Instantiate a mesh with the specified components.
+     *
+     * @param tra if true, include a 3-foot limb for translations, else omit
+     * @param rot if true, include a 4-foot limb for rotations, else omit
+     * @param sca if true, include a 3-foot limb for scales, else omit
+     */
+    public Finial(boolean tra, boolean rot, boolean sca) {
+        this(tra, rot, sca, (float) hpf);
     }
     // *************************************************************************
     // Object methods
 
     /**
-     * Create a shallow copy of this mesh.
+     * Create a copy of this mesh.
      *
      * @return a new mesh, equivalent to this one
      */
@@ -147,14 +202,11 @@ public class Finial extends Mesh {
     /**
      * Populate the buffers.
      *
-     * @param tra if true, include 3 crowsfeet for translations
-     * @param rot if true, include 4 crowsfeet for rotations
-     * @param sca if true, include 3 crowsfeet for scales
+     * @param tra if true, include a 3-foot limb for translations, else omit
+     * @param rot if true, include a 4-foot limb for rotations, else omit
+     * @param sca if true, include a 3-foot limb for scales, else omit
      */
     private void populate(boolean tra, boolean rot, boolean sca) {
-        floats.clear(); // prepare for writing
-        shorts.clear(); // prepare for writing
-
         short sTra = 0, sRot = 0, sSca = 0;
         if (tra) {
             sTra = put3();
@@ -181,98 +233,106 @@ public class Finial extends Mesh {
     }
 
     /**
-     * Add a 3-curve cap to the mesh.
+     * Add a 3-foot limb (for 3 sparklines) to the mesh.
      *
      * @return index of the connecting vertex
      */
     private short put3() {
-        for (int i = 0; i < 7; i++) {
-            putXY(0.0, 0.05 * i);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                putXY(0.0, i * hpf + j * shh);
+            }
         }
         for (int i = 0; i < 3; i++) {
-            putXY(width / 3.0, 0.05 + 0.1 * i);
+            putXY(otw, i * hpf + shh);
         }
-        putXY(width * 2.0 / 3.0, 0.05);
-        putXY(width * 2.0 / 3.0, 0.25);
-        putXY(width, 0.15);
+        putXY(2 * otw, shh);
+        putXY(2 * otw, 2 * hpf + shh);
+        putXY(width, hpf + shh);
         /*
-         * crowsfoot for 1st curve
+         * crowsfoot for 1st sparkline
          */
-        putLine(0, 7);
-        putLine(1, 10);
-        putLine(2, 7);
+        putLine(0, 9);
+        putLine(1, 12);
+        putLine(2, 9);
         /*
-         * crowsfoot for 2nd curve
+         * crowsfoot for 2nd sparkline
          */
-        putLine(2, 8);
-        putLine(3, 12);
-        putLine(4, 8);
+        putLine(3, 10);
+        putLine(4, 14);
+        putLine(5, 10);
         /*
-         * crowsfoot for 3rd curve
+         * crowsfoot for 3rd sparkline
          */
-        putLine(4, 9);
-        putLine(5, 11);
-        putLine(6, 9);
+        putLine(6, 11);
+        putLine(7, 13);
+        putLine(8, 11);
         /*
          * connecting vertical bar
          */
-        putLine(10, 11);
+        putLine(12, 13);
 
-        int result = baseI + 12;
-        height += 0.3;
-        baseI += 13;
+        int connectIndex = baseI + numLines4;
+        height += 3 * hpf;
+        baseI += numVertices3;
 
-        return (short) result;
+        return (short) connectIndex;
     }
 
     /**
-     * Add a 4-curve cap to the mesh.
+     * Add a 4-foot limb (for 4 sparklines) to the mesh.
+     *
+     * @return index of the connecting vertex
      */
     private short put4() {
-        for (int i = 0; i < 9; i++) {
-            putXY(0, 0.05 * i);
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+                putXY(0.0, i * hpf + j * shh);
+            }
         }
         for (int i = 0; i < 4; i++) {
-            putXY(width / 3.0, 0.05 + 0.1 * i);
-            putXY(width * 2.0 / 3.0, 0.05 + 0.1 * i);
+            putXY(otw, i * hpf + shh);
         }
-        putXY(width * 2.0 / 3.0, 0.2);
-        putXY(width, 0.2);
+        for (int i = 0; i < 4; i++) {
+            putXY(2 * otw, i * hpf + shh);
+        }
+        putXY(2 * otw, 2 * hpf);
+        putXY(width, 2 * hpf);
         /*
-         * crowsfoot for 1st curve
+         * crowsfoot for 1st sparkline
          */
-        putLine(0, 9);
-        putLine(1, 10);
-        putLine(2, 9);
+        putLine(0, 12);
+        putLine(1, 16);
+        putLine(2, 12);
         /*
-         * crowsfoot for 2nd curve
+         * crowsfoot for 2nd sparkline
          */
-        putLine(2, 11);
-        putLine(3, 12);
-        putLine(4, 11);
+        putLine(3, 13);
+        putLine(4, 17);
+        putLine(5, 13);
         /*
-         * crowsfoot for 3rd curve
+         * crowsfoot for 3rd sparkline
          */
-        putLine(4, 13);
-        putLine(5, 14);
-        putLine(6, 13);
+        putLine(6, 14);
+        putLine(7, 18);
+        putLine(8, 14);
         /*
-         * crowsfoot for 4th curve
+         * crowsfoot for 4th sparkline
          */
-        putLine(6, 15);
-        putLine(7, 16);
-        putLine(8, 15);
+        putLine(9, 15);
+        putLine(10, 19);
+        putLine(11, 15);
         /*
          * connecting T
          */
-        putLine(10, 16);
-        putLine(17, 18);
+        putLine(16, 19);
+        putLine(20, 21);
 
-        int result = baseI + 18;
-        height += 0.4;
-        baseI += 19;
+        int connectIndex = baseI + 21;
+        height += 4 * hpf;
+        baseI += numVertices4;
 
-        return (short) result;
+        return (short) connectIndex;
     }
 
     /**
@@ -294,8 +354,8 @@ public class Finial extends Mesh {
     /**
      * Add a vertex to the position buffer.
      *
-     * @param x x-offset of the position (&ge;0)
-     * @param y y-offset of the position relative to baseY (&ge;0)
+     * @param x negative x-offset of the position (&ge;0)
+     * @param y negative y-offset of the position relative to baseY (&ge;0)
      */
     private void putXY(double x, double y) {
         assert x >= 0.0 : x;
