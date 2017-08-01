09f560798ff62b6ad564ebd8dad8381bbd4a9fee
Stephen Gold <sgold@sonic.net>
2017-07-31 19:49:03 -0700

    designate version ALPHA+0

--- src/main/java/maud/EditorDialogs.java
+++ src/main/java/maud/EditorDialogs.java
@@ -73,7 +73,8 @@ class EditorDialogs {
         String niftyVersion = nifty.getVersion();
         String text = "Maud, by Stephen Gold\n\nYou are c"
                 + "urrently using Maud, a jMonkeyEngine application for edit"
-                + "ing animated models.\n\nThe version you are using includes "
+                + "ing animated models.\n\nThe version you are using"
+                + "is designated ALPHA+0.\n\nIt includes "
                 + "the following libraries:";
         text += String.format("\n   jme3-core version %s",
                 MyString.quote(JmeVersion.FULL_NAME));
