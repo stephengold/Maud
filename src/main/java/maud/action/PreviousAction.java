408fa69f2da73d91a991b2c7ddb2fc8d29130f38
Stephen Gold <sgold@sonic.net>
2017-09-21 11:00:20 -0700

    split off NextAction and PreviousAction from the EditorInputMode class

---
+++ src/main/java/maud/action/PreviousAction.java
@@ -0,0 +1,130 @@
+/*
+ Copyright (c) 2017, Stephen Gold
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the copyright holder nor the names of its contributors
+ may be used to endorse or promote products derived from this software without
+ specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package maud.action;
+
+import java.util.logging.Logger;
+import maud.Maud;
+import maud.model.EditableCgm;
+import maud.model.EditorModel;
+import maud.model.History;
+
+/**
+ * Process an action string that begins with the word "previous".
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+class PreviousAction {
+    // *************************************************************************
+    // constants and loggers
+
+    /**
+     * message logger for this class
+     */
+    final private static Logger logger
+            = Logger.getLogger(PreviousAction.class.getName());
+    // *************************************************************************
+    // constructors
+
+    /**
+     * A private constructor to inhibit instantiation of this class.
+     */
+    private PreviousAction() {
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Process an action string that begins with the word "previous".
+     *
+     * @param actionString textual description of the action (not null)
+     * @return true if the action is handled, otherwise false
+     */
+    static boolean process(String actionString) {
+        boolean handled = true;
+
+        EditorModel model = Maud.getModel();
+        EditableCgm target = model.getTarget();
+        switch (actionString) {
+            case Action.previousAnimation:
+                target.getAnimation().loadPrevious();
+                break;
+
+            case Action.previousAnimControl:
+                target.getAnimControl().selectPrevious();
+                break;
+
+            case Action.previousBone:
+                target.getBone().selectPrevious();
+                break;
+
+            case Action.previousCheckpoint:
+                History.undo();
+                break;
+
+            case Action.previousJoint:
+                target.getJoint().selectPrevious();
+                break;
+
+            case Action.previousMapping:
+                model.getMap().selectPrevious();
+                break;
+
+            case Action.previousPhysics:
+                target.getPhysics().selectPrevious();
+                break;
+
+            case Action.previousSgc:
+                target.getSgc().selectPrevious();
+                break;
+
+            case Action.previousShape:
+                target.getShape().selectPrevious();
+                break;
+
+            case Action.previousSourceAnimation:
+                model.getSource().getAnimation().loadPrevious();
+                break;
+
+            case Action.previousSourceAnimControl:
+                model.getSource().getAnimControl().selectPrevious();
+                break;
+
+            case Action.previousUserData:
+                target.getUserData().selectPreviousKey();
+                break;
+
+            case Action.previousVertex:
+                target.getVertex().selectPrevious();
+                break;
+
+            default:
+                handled = false;
+        }
+
+        return handled;
+    }
+}
