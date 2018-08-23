4519d35b2dc4cbb5c574c438e82623128b9747e0
Stephen Gold <sgold@sonic.net>
2018-08-22 19:06:21 -0700

    bugfix: incorrect edit count after undo

--- src/main/java/maud/model/cgm/EditableCgm.java
+++ src/main/java/maud/model/cgm/EditableCgm.java
@@ -1526,6 +1526,7 @@ public class EditableCgm extends LoadedCgm {
     @Override
     public EditableCgm clone() throws CloneNotSupportedException {
         EditableCgm clone = (EditableCgm) super.clone();
+        editState = editState.clone();
         return clone;
     }
 
