84a29d31fa5a7a99c4159bfd0c1bf6ac82cc6aa5
Stephen Gold <sgold@sonic.net>
2019-02-14 19:57:04 -0800

    designate version 1.0.0-alpha.8

--- src/main/java/maud/dialog/EditorDialogs.java
+++ src/main/java/maud/dialog/EditorDialogs.java
@@ -119,7 +119,7 @@ public class EditorDialogs {
         String text = "Maud, by Stephen Gold\n\nYou are c"
                 + "urrently using Maud, a jMonkeyEngine application for edit"
                 + "ing animated models.\n\nThe version you are using "
-                //+ "is designated 1.0.0-alpha.7 .\n\nIt "
+                + "is designated v1.0.0-alpha.8 .\n\nIt "
                 + "includes the following libraries:";
         text += String.format("%n   jme3-core version=%s (BSD license)",
                 MyString.quote(JmeVersion.FULL_NAME));
