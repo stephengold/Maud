a4983d682dedf4ea10139e4e1c1d9cf10322716a
Stephen Gold <sgold@sonic.net>
2021-07-07 12:17:19 -0700

    EditorMenus: use HTTPS instead of HTTP

--- src/main/java/maud/menu/EditorMenus.java
+++ src/main/java/maud/menu/EditorMenus.java
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2017-2020, Stephen Gold
+ Copyright (c) 2017-2021, Stephen Gold
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
@@ -294,7 +294,7 @@ public class EditorMenus {
                 EditorDialogs.aboutMaud();
                 break;
             case "JME3 homepage":
-                Heart.browseWeb("http://jmonkeyengine.org/");
+                Heart.browseWeb("https://jmonkeyengine.org/");
                 break;
             case "License":
                 EnumMenus.viewLicense();
