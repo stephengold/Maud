39e7cdf1bcbfd5aa23a6222e41fbc8f4b4d2dfc8
Stephen Gold <sgold@sonic.net>
2021-09-06 00:36:27 -0700

    MeshTool: merge label and button for vertices

--- src/main/java/maud/tool/MeshTool.java
+++ src/main/java/maud/tool/MeshTool.java
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2018-2020, Stephen Gold
+ Copyright (c) 2018-2021, Stephen Gold
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
@@ -274,7 +274,7 @@ class MeshTool extends Tool {
         setButtonText("meshCalculateNormals", calcButton);
         setStatusText("meshElements", elementsText);
         setButtonText("meshMode", modeButton);
-        setStatusText("meshVertices", verticesText);
+        setButtonText("meshVertices", verticesText);
         setButtonText("meshWeights", weightsButton);
     }
 
