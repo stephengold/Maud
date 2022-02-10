e4146bcab1230cde73292938b5170cfaad4ac973
Stephen Gold <sgold@sonic.net>
2022-02-09 22:51:25 -0800

    BoneMenus:  delete an unnecessary import

--- src/main/java/maud/menu/BoneMenus.java
+++ src/main/java/maud/menu/BoneMenus.java
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2017-2021, Stephen Gold
+ Copyright (c) 2017-2022, Stephen Gold
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
@@ -33,7 +33,6 @@ import jme3utilities.MyString;
 import maud.Maud;
 import maud.action.ActionPrefix;
 import maud.dialog.EditorDialogs;
-import maud.menu.ShowMenus;
 import maud.model.EditorModel;
 import maud.model.cgm.Cgm;
 import maud.model.cgm.EditableCgm;
