65602aaaf343675cb41c9166aacfca69b2dc2cdb
stephengold <sgold@sonic.net>
2025-07-05 14:34:24 -0700

    obtain snapshot artifacts from Central Portal instead of OSSRH

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,8 +7,10 @@ dependencyResolutionManagement {
         //mavenLocal() // to find libraries installed locally
         mavenCentral() // to find libraries released to the Maven Central repository
         maven { url = uri("https://jitpack.io") } // to find jme3_xbuf_loader
-        //maven { url = uri("https://s01.oss.sonatype.org/content/groups/staging") } // to find libraries staged but not yet released
-        //maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots") } // to find public snapshots of libraries
+        maven {
+            name = "Central Portal Snapshots"
+            url = uri("https://central.sonatype.com/repository/maven-snapshots/")
+        }
     }
 }
 
